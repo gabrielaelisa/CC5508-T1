3938a58362b508f3177cd0e36b2d46479d423e83
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2018-10-01 23:44:27 -0300

    finding bounding box

--- Component.py
+++ Component.py
@@ -15,3 +15,34 @@ class Component:
         :return: void
         '''
         self.points.append(x)
+
+    def find_box(self):
+        x_min =self.points[0][0]
+        x_max= x_min
+        y_min= self.points[0][1]
+        y_max= y_min
+        for (x,y) in self.points:
+            if x<x_min:
+                x_min= x
+            if x>x_max:
+                x_max=x
+            if y< y_min:
+                y_min=y
+            if y> y_max:
+                y_max=y
+        # initialpoint for bondingbox
+        # esquina superior derecha
+        # todo
+        # checkear que este dentro de los indices de la imagen
+        xi = x_min-1
+        yi= y_max +1
+        width= x_max +1 -(x_min-1)
+        heigth= y_max +1 -(y_min -1)
+        self.boundingbox.extend([xi, yi, width, heigth])
+        print(self.boundingbox)
+
+com = Component(1)
+com.points= [(1,2),(2,3),(3,4),(4,5)]
+com.find_box()
+
+
